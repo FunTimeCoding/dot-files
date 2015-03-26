f48ee99d39c2ea68825df0c84ea459eea74d4180
Alexander Reitzel <funtimecoding@gmail.com>
2015-03-26 16:51:46 +0100

    add plantuml wrapper script

---
+++ bin/plantuml.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+JAR_DIR="$HOME/.local/jar"
+if [ ! -d "${JAR_DIR}" ]; then
+    mkdir -p "${JAR_DIR}"
+fi
+
+FILE="${JAR_DIR}/plantuml.jar"
+if [ ! -f "${FILE}" ]; then
+    wget http://downloads.sourceforge.net/project/plantuml/plantuml.jar -O "${FILE}"
+fi
+
+java -jar ${FILE} -tsvg $@
