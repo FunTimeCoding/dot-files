99e482175c744559359c3e2fc6195c55e70a831a
Alexander Reitzel <funtimecoding@gmail.com>
2015-05-08 00:17:25 +0200

    composer update, add blank lines to setup.script

--- setup.sh
+++ setup.sh
@@ -1,36 +1,39 @@
 #!/bin/sh -ex
 
-SCRIPT_DIR="$(cd $(dirname ${0}); pwd)"
+SCRIPT_DIR=$(cd "$(dirname "${0}")"; pwd)
 WORK_DIR="${HOME}/.dotfiles"
 
 if [ ! "${WORK_DIR}" = "${SCRIPT_DIR}" ]; then
     echo "Dotfiles directory must be ${WORK_DIR}."
+
     exit 1
 fi
 
 SSH_DIR="${HOME}/.ssh"
+
 if [ -d "${SSH_DIR}" ]; then
     echo "Directory ${SSH_DIR} already exists. Replace it? (y/n) This will rm -rf ${SSH_DIR}."
     read OPT
+
     case ${OPT} in
         y)
             echo "Deleting ${SSH_DIR}."
             rm -rf "${SSH_DIR}"
-            break
             ;;
         n)
             echo "Setup canceled."
+
             exit 0
             ;;
         *)
             echo "Invalid choice."
-            exit 0
+
+            exit 1
             ;;
     esac
 fi
 
 echo "Creating and updating symlinks."
-
 FILES=$(find "${SCRIPT_DIR}" -maxdepth 1 -name '.*' -not -path '*.git' -and -not -path "${WORK_DIR}" | awk -F/ '{print $NF}')
 
 for FILE in ${FILES}; do
@@ -39,6 +42,7 @@ done
 
 mkdir -p "${HOME}/.config"
 ln -snf "${WORK_DIR}/powerline" "${HOME}/.config/powerline"
+
 if type pip > /dev/null; then
     OS=$(uname)
     if [ "${OS}" = "Linux" ]; then
@@ -51,24 +55,28 @@ else
 fi
 
 NEOBUNDLE_PATH="${HOME}/.vim/bundle/neobundle.vim"
+
 if [ ! -d "${NEOBUNDLE_PATH}" ]; then
     echo "Downloading NeoBundle."
     git clone https://github.com/Shougo/neobundle.vim "${NEOBUNDLE_PATH}"
 fi
 
 LOCAL_BIN_DIR="${HOME}/.local/bin"
+
 if [ ! -d "${LOCAL_BIN_DIR}" ]; then
     echo "Setting up ${LOCAL_BIN_DIR}."
     mkdir -p "${LOCAL_BIN_DIR}"
 fi
 
 OH_MY_ZSH_DIR="${HOME}/.oh-my-zsh"
+
 if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
     echo "Setting up ${OH_MY_ZSH_DIR}."
     git clone git://github.com/robbyrussell/oh-my-zsh.git "${OH_MY_ZSH_DIR}"
 fi
 
 ZSH_SYNTAX_HIGHLIGHTING="${OH_MY_ZSH_DIR}/custom/plugins/zsh-syntax-highlighting/"
+
 if [ ! -d "${ZSH_SYNTAX_HIGHLIGHTING}" ]; then
     echo "downloading zsh-syntax-highlighting"
     git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_SYNTAX_HIGHLIGHTING}"
@@ -76,28 +84,33 @@ fi
 
 if ! type php > /dev/null; then
     echo "php is not installed, not installing composer"
+
     exit 0
 fi
 
 COMPOSER_BIN="${LOCAL_BIN_DIR}/composer"
+
 if [ ! -f "${COMPOSER_BIN}" ]; then
     echo "Downloading composer."
     curl -sS https://getcomposer.org/installer | php -- --install-dir="${LOCAL_BIN_DIR}" --filename=composer
 fi
 
 PHP_TOOLS_DIR="${HOME}/.local/php/"
+
 if [ ! -d "${PHP_TOOLS_DIR}" ]; then
     echo "Creating php tools directory."
     mkdir -p "${PHP_TOOLS_DIR}"
 fi
 
 PHP_TOOLS_CONFIG="${HOME}/.local/php/composer.json"
+
 if [ ! -f "${PHP_TOOLS_CONFIG}" ]; then
     echo "Creating symlink for composer.json."
     ln -snf "${WORK_DIR}/composer.json" "${PHP_TOOLS_CONFIG}"
 fi
 
 PHP_TOOLS_COMPOSER_LOCK="${HOME}/.local/php/composer.lock"
+
 if [ ! -f "${PHP_TOOLS_COMPOSER_LOCK}" ]; then
     echo "Installing php tools using composer"
     cd "${PHP_TOOLS_DIR}"
