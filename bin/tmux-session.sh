1c754c56703aac976f611a5b8d09a17a933058e3
Alexander Reitzel <funtimecoding@gmail.com>
2015-06-19 19:22:42 +0200

    run tmux with -2, reformat powerline config, add ascii config when powerline symbols arent available

--- bin/tmux-session.sh
+++ bin/tmux-session.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+
 [ "${TMUX}" = "" ] || exit 0
 
-has_session () {
+has_session()
+{
     tmux has-session -t $1 2>/dev/null
 }
 
+TMUX_CMD="tmux -2"
 DEFAULT_NAME="_default"
 
 if ! has_session "${DEFAULT_NAME}"; then
-    tmux new-session -s "${DEFAULT_NAME}" -d
+    ${TMUX_CMD} new-session -s "${DEFAULT_NAME}" -d
 fi
 
 PS3="Select session: "
@@ -20,11 +23,11 @@ do
     case ${OPTION} in
         "NEW")
             read -p "Enter new session name: " SESSION_NAME
-            tmux new-session -s "${SESSION_NAME}"
+            ${TMUX_CMD} new-session -s "${SESSION_NAME}"
             break
             ;;
         *)
-            tmux attach-session -t "${OPTION}"
+            ${TMUX_CMD} attach-session -t "${OPTION}"
             break
             ;;
     esac
