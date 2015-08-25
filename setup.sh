935a936fd793e2a81229b968ba3351ef26bb8f88
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-25 18:22:00 +0200

    use http links for git clones, add a few more aliases, dont use ing form in setup script output

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ if [ -d "${SSH_DIR}" ]; then
 
     case ${OPT} in
         y)
-            echo "Deleting ${SSH_DIR}."
+            echo "Delete ${SSH_DIR}."
             rm -rf "${SSH_DIR}"
             ;;
         n)
@@ -34,7 +34,7 @@ if [ -d "${SSH_DIR}" ]; then
     esac
 fi
 
-echo "Updating symlinks."
+echo "Update symlinks."
 FILES=$(find "${SCRIPT_DIR}" -maxdepth 1 -name '.*' -not -path '*.git' -and -not -path "${WORK_DIR}" | awk -F/ '{print $NF}')
 
 for FILE in ${FILES}; do
@@ -52,7 +52,7 @@ if command -v pip3 >/dev/null 2>&1; then
         pip3 install -U powerline-status
     fi
 else
-    echo "pip3 is not installed, falling back to pip2"
+    echo "pip3 is not installed, fall back to pip2."
 
     if command -v pip2 >/dev/null 2>&1; then
         if [ "${OS}" = "Linux" ]; then
@@ -61,40 +61,40 @@ else
             pip2 install -U powerline-status
         fi
     else
-        echo "pip2 is not installed, not installing powerline"
+        echo "pip2 is not installed, powerline not installed."
     fi
 fi
 
 NEOBUNDLE_PATH="${HOME}/.vim/bundle/neobundle.vim"
 
 if [ ! -d "${NEOBUNDLE_PATH}" ]; then
-    echo "Downloading NeoBundle."
+    echo "Clone NeoBundle."
     git clone https://github.com/Shougo/neobundle.vim "${NEOBUNDLE_PATH}"
 fi
 
 LOCAL_BIN_DIR="${HOME}/.local/bin"
 
 if [ ! -d "${LOCAL_BIN_DIR}" ]; then
-    echo "Setting up ${LOCAL_BIN_DIR}."
+    echo "Set up ${LOCAL_BIN_DIR}."
     mkdir -p "${LOCAL_BIN_DIR}"
 fi
 
 OH_MY_ZSH_DIR="${HOME}/.oh-my-zsh"
 
 if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
-    echo "Setting up ${OH_MY_ZSH_DIR}."
-    git clone git://github.com/robbyrussell/oh-my-zsh.git "${OH_MY_ZSH_DIR}"
+    echo "Clone oh-my-zsh."
+    git clone https://github.com/robbyrussell/oh-my-zsh "${OH_MY_ZSH_DIR}"
 fi
 
 ZSH_SYNTAX_HIGHLIGHTING="${OH_MY_ZSH_DIR}/custom/plugins/zsh-syntax-highlighting/"
 
 if [ ! -d "${ZSH_SYNTAX_HIGHLIGHTING}" ]; then
-    echo "downloading zsh-syntax-highlighting"
-    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_SYNTAX_HIGHLIGHTING}"
+    echo "Clone zsh-syntax-highlighting."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_SYNTAX_HIGHLIGHTING}"
 fi
 
 if command -v php >/dev/null 2>&1; then
-    echo "PHP is not installed, not installing Composer."
+    echo "PHP is not installed, Composer not installed."
 
     exit 0
 fi
@@ -102,28 +102,28 @@ fi
 COMPOSER_BIN="${LOCAL_BIN_DIR}/composer"
 
 if [ ! -f "${COMPOSER_BIN}" ]; then
-    echo "Downloading Composer."
+    echo "Install Composer."
     ${CURL} -sS https://getcomposer.org/installer | php -- --install-dir="${LOCAL_BIN_DIR}" --filename=composer
 fi
 
 COMPOSER_DIR="${HOME}/.composer"
 
 if [ ! -d "${COMPOSER_DIR}" ]; then
-    echo "Creating Composer directory."
+    echo "Create Composer directory."
     mkdir -p "${COMPOSER_DIR}"
 fi
 
 COMPOSER_CONFIG="${HOME}/.composer/composer.json"
 
 if [ ! -f "${COMPOSER_CONFIG}" ] || [ ! -L "${COMPOSER_CONFIG}" ]; then
-    echo "Symlinking composer.json."
+    echo "Symlink composer.json."
     ln -snf "${WORK_DIR}/composer.json" "${COMPOSER_CONFIG}"
 fi
 
 COMPOSER_LOCK="${HOME}/.composer/composer.lock"
 
 if [ ! -f "${COMPOSER_LOCK}" ]; then
-    echo "Installing Composer packages."
+    echo "Install Composer packages."
     cd "${COMPOSER_DIR}"
     ${COMPOSER_BIN} self-update
     ${COMPOSER_BIN} install
