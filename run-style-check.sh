a777caebdae63c5d6247468fae82689e7630a7c2
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-01 16:23:12 +0200

    check for existence of shellcheck script before starting

--- run-style-check.sh
+++ run-style-check.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 
+if [ ! -x "$(command -v shellcheck)" ]; then
+    echo "Command not found: shellcheck"
+
+    exit 1
+fi
+
 find . \( -name '*.sh' -and -not -path '*/.vim/*' \) -exec sh -c 'shellcheck ${1} || true' '_' '{}' \;
