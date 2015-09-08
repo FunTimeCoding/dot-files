d3a27fda6e287107bc987cb818ba1c868f687e5b
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-08 10:29:06 +0200

    fix logic issue where dist upgrade could not be reached when no normal updates are available

--- bin/debian-update.sh
+++ bin/debian-update.sh
@@ -6,31 +6,30 @@ OUTPUT=$(sudo apt-get -s upgrade)
 GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
 
 if [ "${GREP}" = "" ]; then
-    echo "Nothing to update."
+    echo "Nothing to update (upgrade)."
+else
+    if [ ! "${1}" = "--yes" ]; then
+        echo "Available updates:"
+        echo "${OUTPUT}"
+        HOST_NAME=$(hostname)
+        echo "Update ${HOST_NAME}? [y/n]"
+        read READ
 
-    exit 0
-fi
-
-if [ ! "${1}" = "--yes" ]; then
-    echo "Available updates:"
-    echo "${OUTPUT}"
-    HOST_NAME=$(hostname)
-    echo "Update ${HOST_NAME}? [y/n]"
-    read READ
-
-    if [ ! "${READ}" = "y" ]; then
-        exit 0
+        if [ ! "${READ}" = "y" ]; then
+            exit 0
+        fi
     fi
+
+    sudo apt-get -qq upgrade
+    sudo apt-get -qq autoremove
+    sudo apt-get -qq clean
 fi
 
-sudo apt-get -qq upgrade
-sudo apt-get -qq autoremove
-sudo apt-get -qq clean
 OUTPUT=$(sudo apt-get -s dist-upgrade)
 GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
 
 if [ "${GREP}" = "" ]; then
-    echo "Nothing to update."
+    echo "Nothing to update (dist-upgrade)."
 
     exit 0
 fi
