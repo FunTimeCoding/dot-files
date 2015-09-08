545b505b0bf1004763dc07fbf27c8e59449b5341
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-08 10:25:27 +0200

    do dist-upgrade in debian-update script

--- bin/debian-update.sh
+++ bin/debian-update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
 echo "Check for Debian updates."
-sudo apt-get update -qq
-OUTPUT=$(sudo apt-get upgrade -s)
+sudo apt-get -qq update
+OUTPUT=$(sudo apt-get -s upgrade)
 GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
 
 if [ "${GREP}" = "" ]; then
@@ -23,5 +23,30 @@ if [ ! "${1}" = "--yes" ]; then
     fi
 fi
 
-sudo apt-get upgrade -qq
-sudo apt-get clean
+sudo apt-get -qq upgrade
+sudo apt-get -qq autoremove
+sudo apt-get -qq clean
+OUTPUT=$(sudo apt-get -s dist-upgrade)
+GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
+
+if [ "${GREP}" = "" ]; then
+    echo "Nothing to update."
+
+    exit 0
+fi
+
+if [ ! "${1}" = "--yes" ]; then
+    echo "Available updates:"
+    echo "${OUTPUT}"
+    HOST_NAME=$(hostname)
+    echo "Update ${HOST_NAME}? [y/n]"
+    read READ
+
+    if [ ! "${READ}" = "y" ]; then
+        exit 0
+    fi
+fi
+
+sudo apt-get -qq dist-upgrade
+sudo apt-get -qq autoremove
+sudo apt-get -qq clean
