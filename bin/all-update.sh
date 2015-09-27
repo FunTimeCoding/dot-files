6e29145990d8fae5a7e7e0677ec30e16b4b79d57
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-27 23:23:09 +0200

    put vim update first to not clear previous output

--- bin/all-update.sh
+++ bin/all-update.sh
@@ -2,13 +2,13 @@
 
 echo "Run all updates."
 OS=$(uname)
+vim-update.sh
 brew-update.sh "${1}"
 gem-update.sh "${1}"
 npm-update.sh "${1}"
 pip2-update.sh "${1}"
 pip3-update.sh "${1}"
 atom-update.sh "${1}"
-vim-update.sh
 cabal-update.sh
 
 if [ "${OS}" = "Darwin" ]; then
