3f6c517826a156e9f3da743d2904fe71b0138c4d
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-18 08:16:57 +0100

    extend tmux.sh to optionally create a four-panel window

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -9,15 +9,15 @@ has_session()
     tmux has-session -t "${1}" 2>/dev/null
 }
 
-TMUX_CMD="tmux -2"
+TMUX_COMMAND="tmux -2"
 DEFAULT_NAME="1"
 
 if ! has_session "${DEFAULT_NAME}"; then
-    ${TMUX_CMD} new-session -s "${DEFAULT_NAME}" -d
+    ${TMUX_COMMAND} new-session -s "${DEFAULT_NAME}" -d
 fi
 
 OPTIONS=$(tmux list-sessions -F "#S" | tr '\n' ' ')
-OPTIONS="${OPTIONS} new"
+OPTIONS="${OPTIONS} quad new"
 echo "Select session from: ${OPTIONS}"
 read -r OPTION
 
@@ -25,9 +25,21 @@ case ${OPTION} in
     new)
         echo "Enter new session name:"
         read -r SESSION_NAME
-        ${TMUX_CMD} new-session -s "${SESSION_NAME}"
+        ${TMUX_COMMAND} new-session -s "${SESSION_NAME}"
+        ;;
+    quad)
+        if ! has_session "${OPTION}"; then
+            ${TMUX_COMMAND} new-session -s "${OPTION}" -d
+            ${TMUX_COMMAND} split-window -v
+            ${TMUX_COMMAND} split-window -h
+            ${TMUX_COMMAND} select-pane -t 0
+            ${TMUX_COMMAND} split-window -h
+            ${TMUX_COMMAND} select-pane -t 0
+        fi
+
+        ${TMUX_COMMAND} attach-session -t "${OPTION}"
         ;;
     *)
-        ${TMUX_CMD} attach-session -t "${OPTION}"
+        ${TMUX_COMMAND} attach-session -t "${OPTION}"
         ;;
 esac
