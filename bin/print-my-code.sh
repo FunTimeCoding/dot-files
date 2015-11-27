fda0a71e3850992d9a48c5b460009fefc5b79b5f
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-27 19:10:49 +0100

    make print-my-code script work on linux instead

--- bin/print-my-code.sh
+++ bin/print-my-code.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-find -E "${HOME}/Code/Personal" -type f -size -4096c ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$' -exec sh -c 'grep -Hn "${2}" "${1}"' '_' '{}' "${1}" \;
+find "${HOME}/Code/Personal" -type f -size -4096c -regextype posix-extended ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$' -exec sh -c 'grep -Hn "${2}" "${1}"' '_' '{}' "${1}" \;
