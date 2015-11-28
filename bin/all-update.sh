64ae9b202c54bc27d4b7da7d269554641df1b6ce
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-28 12:38:11 +0100

    reorder all-update script so it can run on linux too

--- bin/all-update.sh
+++ bin/all-update.sh
@@ -3,19 +3,18 @@
 echo "Run all updates."
 OS=$(uname)
 vim-update.sh
-brew-update.sh "${1}"
-gem-update.sh "${1}"
-#npm-update.sh "${1}" # NPM has an issue when updating from a script instead of an interactive shell.
 pip2-update.sh "${1}"
 pip3-update.sh "${1}"
 #atom-update.sh "${1}" # No plugins used, no update needed.
-cabal-update.sh
 
 if [ "${OS}" = "Darwin" ]; then
+    brew-update.sh "${1}"
+    #npm-update.sh "${1}" # NPM has an issue when updating from a script instead of an interactive shell.
+    gem-update.sh "${1}"
     osx-update.sh "${1}"
+    cabal-update.sh
+    cd "${HOME}/Code" || exit 1
+    repo-update.rb -d 2
 elif [ "${OS}" = "Linux" ]; then
     debian-update.sh "${1}"
 fi
-
-cd "${HOME}/Code" || exit 1
-repo-update.rb -d 2
