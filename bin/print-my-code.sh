661c50a89cf38e350e3bb457ac8d75bcaeb0c3b3
Alexander Reitzel <funtimecoding@gmail.com>
2015-11-28 14:41:57 +0100

    fix print-my-code script to use gfind on osx

--- bin/print-my-code.sh
+++ bin/print-my-code.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -e
 
-find "${HOME}/Code/Personal" -type f -size -4096c -regextype posix-extended ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$' -exec sh -c 'grep -Hn "${2}" "${1}"' '_' '{}' "${1}" \;
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
+    FIND="gfind"
+else
+    FIND="find"
+fi
+
+${FIND} "${HOME}/Code/Personal" -type f -size -4096c -regextype posix-extended ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$' -exec sh -c 'grep -Hn "${2}" "${1}"' '_' '{}' "${1}" \;
