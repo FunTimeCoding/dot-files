fd8a68e9a1e257dcc33cc15be4d5ecdc43bed7bf
Alexander Reitzel <funtimecoding@gmail.com>
2016-01-11 09:53:36 +0100

    do autoremove and cleanup on both upgrade steps in debian-update.sh

--- bin/debian-update.sh
+++ bin/debian-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 
-echo "Check for Debian updates."
+echo "Check for Debian updates (apt-get upgrade)."
 sudo apt-get -qq update
 OUTPUT=$(sudo apt-get -s upgrade)
 GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
 
 if [ "${GREP}" = "" ]; then
-    echo "Nothing to update (upgrade)."
+    echo "Nothing to update (apt-get upgrade)."
 else
     if [ ! "${1}" = "--yes" ]; then
         echo "Available updates:"
@@ -26,28 +26,27 @@ else
     sudo apt-get -qq clean
 fi
 
+echo "Check for Debian updates (apt-get dist-upgrade)."
 OUTPUT=$(sudo apt-get -s dist-upgrade)
 GREP=$(echo "${OUTPUT}" | grep 'The following packages will be upgraded:') || GREP=""
 
 if [ "${GREP}" = "" ]; then
-    echo "Nothing to update (dist-upgrade)."
+    echo "Nothing to update (apt-get dist-upgrade)."
+else
+    if [ ! "${1}" = "--yes" ]; then
+        echo "Available updates:"
+        echo "${OUTPUT}"
+        HOST_NAME=$(hostname)
+        echo "Update ${HOST_NAME}? [y/n]"
+        read -r READ
 
-    exit 0
-fi
+        if [ ! "${READ}" = "y" ]; then
+            exit 0
+        fi
 
-if [ ! "${1}" = "--yes" ]; then
-    echo "Available updates:"
-    echo "${OUTPUT}"
-    HOST_NAME=$(hostname)
-    echo "Update ${HOST_NAME}? [y/n]"
-    read -r READ
-
-    if [ ! "${READ}" = "y" ]; then
-        exit 0
+        sudo apt-get -qq dist-upgrade
+        echo "Run autoremove and clean."
+        sudo apt-get -qq autoremove
+        sudo apt-get -qq clean
     fi
 fi
-
-sudo apt-get -qq dist-upgrade
-echo "Run autoremove and clean."
-sudo apt-get -qq autoremove
-sudo apt-get -qq clean
