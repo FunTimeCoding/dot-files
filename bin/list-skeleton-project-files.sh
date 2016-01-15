4dc2a7e5bf21b72568d4ab10e707fada030171b4
Alexander Reitzel <funtimecoding@gmail.com>
2016-01-15 14:29:30 +0100

    make list-todos linux compatible

--- bin/list-skeleton-project-files.sh
+++ bin/list-skeleton-project-files.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -e
 
-find -E "${HOME}/Code/Personal" -type f -path '*-skeleton\/*' -and ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$'
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
+    FIND="gfind"
+else
+    FIND="find"
+fi
+
+${FIND} "${HOME}/Code/Personal" -regextype posix-extended -type f -path '*-skeleton\/*' -and ! -regex '^.*/(build|node_modules|bower_components|vendor|\.git|\.idea|\.pyvenv)/.*$'
--- list-todos.sh
+++ list-todos.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -e
 
-find -E . -type f -and ! -regex '^.*/(\.git|\.vim)/.*$' -exec sh -c 'grep -rn TODO ${1} | grep -v ${2}' '_' '{}' '${0}' \;
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
+    FIND="gfind"
+else
+    FIND="find"
+fi
+
+${FIND} . -regextype posix-extended -type f -and ! -regex '^.*/(\.git|\.vim)/.*$' -exec sh -c 'grep -rn TODO ${1} | grep -v ${2}' '_' '{}' '${0}' \;
