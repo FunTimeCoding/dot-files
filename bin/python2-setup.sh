4ba9dd1028f561f2aae61a4c045aa345d7b4c1c9
Alexander Reitzel <funtimecoding@gmail.com>
2016-01-30 16:59:04 +0100

    install ansible instead of powerline in python2

--- bin/python2-setup.sh
+++ bin/python2-setup.sh
@@ -2,5 +2,5 @@
 
 python2 -m ensurepip --user
 pip2 install --upgrade --user pip setuptools
-pip2 install --upgrade --user powerline-status
+pip2 install --upgrade --user ansible
 rm "${HOME}/.local/bin/pip"
