28c41c90ae65bd7d757f9a4a79deb62c40f4c9ae
Alexander Reitzel <funtimecoding@gmail.com>
2016-04-18 11:46:06 +0200

    use gunzip in get-ruby-version only on linux

--- bin/get-ruby-version.sh
+++ bin/get-ruby-version.sh
@@ -6,7 +6,14 @@ if [ "$(command -v hxselect || true)" = "" ]; then
     exit 1
 fi
 
-OUTPUT=$(wget --quiet https://cache.ruby-lang.org/pub/ruby --output-document - | gunzip)
+OPERATING_SYSTEM=$(uname)
+
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
+    OUTPUT=$(wget --quiet https://cache.ruby-lang.org/pub/ruby --output-document - | gunzip)
+else
+    OUTPUT=$(wget --quiet https://cache.ruby-lang.org/pub/ruby --output-document -)
+fi
+
 LIST=$(echo ${OUTPUT} | hxselect -s "\n" -c a 2>/dev/null | grep '^ruby-[0-9].*') || true
 LATEST=""
 
