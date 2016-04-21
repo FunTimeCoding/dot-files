cbfb4d430fdab173e8844cf940e285bf85baec1d
Alexander Reitzel <funtimecoding@gmail.com>
2016-04-21 14:47:30 +0200

    add script to say done

---
+++ bin/done.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+INITIAL_STATE=$(osascript -e 'output muted of (get volume settings)')
+
+if [ "${INITIAL_STATE}" = false ]; then
+    osascript -e 'set volume output muted false'
+fi
+
+osascript -e 'set volume output volume 50'
+say done
+
+if [ "${INITIAL_STATE}" = false ]; then
+    osascript -e 'set volume output muted true'
+fi
