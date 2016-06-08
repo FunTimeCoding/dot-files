19adbaa74d2ebcc5292c440e6fee58b2052b4989
Alexander Reitzel <funtimecoding@gmail.com>
2016-06-08 12:55:18 +0200

    fix done.sh mute/unmute when already muted

--- bin/done.sh
+++ bin/done.sh
@@ -2,13 +2,13 @@
 
 INITIAL_STATE=$(osascript -e 'output muted of (get volume settings)')
 
-if [ "${INITIAL_STATE}" = false ]; then
+if [ "${INITIAL_STATE}" = true ]; then
     osascript -e 'set volume output muted false'
 fi
 
 osascript -e 'set volume output volume 50'
 say done
 
-if [ "${INITIAL_STATE}" = false ]; then
+if [ "${INITIAL_STATE}" = true ]; then
     osascript -e 'set volume output muted true'
 fi
