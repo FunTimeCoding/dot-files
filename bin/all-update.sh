7f091b49575b8ebdf42b163d1390bf397f98a5e0
Alexander Reitzel <funtimecoding@gmail.com>
2018-04-16 11:15:55 +0200

    use singular form directory convention

--- bin/all-update.sh
+++ bin/all-update.sh
@@ -4,12 +4,12 @@ SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Darwin ]; then
     brew-update.sh
-    pip3-update.sh
-    pip2-update.sh
-    gem-update.sh
     brew-cask-update.sh
     mas-update.sh
     macos-update.sh
+    pip3-update.sh
+    pip2-update.sh
+    gem-update.sh
 elif [ -f /etc/debian_version ]; then
     "${HOME}/src/debian-tools/bin/update.sh"
 elif [ -f /etc/arch-release ]; then
@@ -17,4 +17,4 @@ elif [ -f /etc/arch-release ]; then
 fi
 
 cd "${HOME}/src/ansible"
-bin/play.sh playbooks/repositories.yaml
+bin/play.sh playbook/repositories.yaml
