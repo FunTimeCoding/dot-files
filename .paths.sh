6536d691a86c0fd6032c3f4d2690a3197afe621f
Alexander Reitzel <funtimecoding@gmail.com>
2019-01-15 16:24:15 +0100

    reduce paths

--- .paths.sh
+++ .paths.sh
@@ -12,8 +12,6 @@ ${HOME}/opt/git-2.11.0/bin
 ${HOME}/opt/multitail-6.4.2/bin
 ${HOME}/opt/lua-5.3.2/bin
 ${HOME}/opt/tidy-5.1.25/bin
-${HOME}/opt/node-5.8.0/bin
-${HOME}/opt/node-5.9.1/bin
 ${HOME}/opt/node-6.2.0/bin
 ${HOME}/opt/neovim/bin
 ${HOME}/opt/tmux-2.1/bin
@@ -36,8 +34,6 @@ ${HOME}/.cabal/bin
 ${HOME}/.composer/vendor/bin
 ${HOME}/.stack/snapshots/x86_64-linux/lts-5.17/7.10.3/bin
 ${HOME}/.stack/programs/x86_64-linux/ghc-7.10.3/bin
-${HOME}/Code/Work/development-tools/bin
-${HOME}/Code/Foreign/arcanist/bin
 ${HOME}/src/dot-files/bin
 ${HOME}/bin
 ${HOME}/src/arcanist/bin"
